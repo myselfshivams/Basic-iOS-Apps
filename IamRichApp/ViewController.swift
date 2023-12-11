bdbd099620d8a186ade30335f350d0449cc7d202
Shivam <143623476+myselfshivams@users.noreply.github.com>
2023-12-11 19:14:41 +0530

    Add files via upload

---
+++ IamRichApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Shivam on 11/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
